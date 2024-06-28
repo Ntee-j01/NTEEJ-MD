59a7c7e2d19b6290b953284dc9212d6d503cb59a
Nteej <140890199+Ntee-j01@users.noreply.github.com>
2024-06-28 18:24:36 +0200

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -1,3 +1,4 @@
+//nteej
 let baseApi = process.env.API_SMD || global.api_smd || "https://api-smd-1.vercel.app";
 const {
   tlang,
@@ -267,7 +268,7 @@ smd({
   try {
     let _0x59e849 = _0x1c586e ? _0x1c586e : _0x1ae8f8.reply_message ? _0x1ae8f8.reply_message.text : "";
     if (!_0x1c586e) {
-      return await _0x1ae8f8.reply("*Provide Repo Url, _.gitclone https://github.com/DeeCeeXxx/QUEEN_ANITA-V2_*");
+      return await _0x1ae8f8.reply("*Provide Repo Url, _.gitclone https://github.com/Ntee-j01/NTEEJ-MD_*");
     }
     const _0x5906ab = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
     if (!_0x5906ab.test(_0x1c586e)) {
@@ -301,7 +302,7 @@ smd({
   try {
     let _0x204f81 = _0x55aba2.reply_text ? _0x55aba2.reply_text : _0x56da6b;
     if (!_0x204f81) {
-      return _0x55aba2.reply("*_Example : .tts Hi,I am QUEEN_ANITA-V2 whatsapp bot._*");
+      return _0x55aba2.reply("*_Example : .tts Hi,I am NTEEJ-MD_ whatsapp bot._*");
     }
     try {
       let _0x1974d5 = _0x56da6b ? _0x56da6b.split(" ")[0].toLowerCase() : "en";
@@ -316,7 +317,7 @@ smd({
         },
         mimetype: "audio/mpeg",
         ptt: true,
-        fileName: "QUEEN_ANITA-V2-tts.m4a"
+        fileName: "NTEEJ-MD-tts.m4a"
       }, {
         quoted: _0x55aba2
       });
@@ -332,7 +333,7 @@ smd({
         },
         mimetype: "audio/mpeg",
         ptt: true,
-        fileName: "QUEEN_ANITA-V2-tts.m4a"
+        fileName: "NTEEJ-MD-tts.m4a"
       }, {
         quoted: _0x55aba2
       });
@@ -440,7 +441,7 @@ smd({
       var _0x3048ab = {
         ...(await _0xe5ea97.bot.contextInfo(Config.botname, "ᴠɪᴅᴇᴏ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ"))
       };
-      let _0x3448d2 = _0x4e4465.title || _0x3e3caf || _0x421809[1] || "Suhail MD -- YT Video";
+      let _0x3448d2 = _0x4e4465.title || _0x3e3caf || _0x421809[1] || "NTEEJ-MD -- YT Video";
       if (_0x3e3caf) {
         await _0xe5ea97.bot.sendMessage(_0xe5ea97.chat, {
           [_0x5b10f0]: {
@@ -595,7 +596,7 @@ smd({
     };
     let _0x4737bb = {
       audio: _0x2ba501,
-      fileName: "QUEEN_ANITA-V2 tiktok Sound" + _0x19c223 + ".m4a",
+      fileName: "NTEEJ-MD tiktok Sound" + _0x19c223 + ".m4a",
       mimetype: "audio/mpeg",
       ptt: true,
       contextInfo: _0x29fdd9
@@ -760,7 +761,7 @@ smd({
     }
     let _0x3b2ca6 = await yts(_0x4ec99f);
     let _0x4123ae = _0x3b2ca6.all[0];
-    let _0x5883a9 = "\t *QUEEN_ANITA-V2 • SONG DOWNLOADER*   \n\n*Title :* " + _0x4123ae.title + "\nUrl : " + _0x4123ae.url + "\n*Description :* " + _0x4123ae.timestamp + "\n*Views :* " + _0x4123ae.views + "\n*Uploaded :* " + _0x4123ae.ago + "\n*Author :* " + _0x4123ae.author.name + "\n\n\n_Reply 1 To Video_ Or _1 document_\n_Reply 2 To Audio_ Or _2 document_";
+    let _0x5883a9 = "\t *NTEEJ-MD• SONG DOWNLOADER*   \n\n*Title :* " + _0x4123ae.title + "\nUrl : " + _0x4123ae.url + "\n*Description :* " + _0x4123ae.timestamp + "\n*Views :* " + _0x4123ae.views + "\n*Uploaded :* " + _0x4123ae.ago + "\n*Author :* " + _0x4123ae.author.name + "\n\n\n_Reply 1 To Video_ Or _1 document_\n_Reply 2 To Audio_ Or _2 document_";
     let _0x3885cc = await smdBuffer(_0x4123ae.thumbnail);
     var _0x44a363 = {
       ...(await _0x2c2023.bot.contextInfo(Config.botname, "ʏᴏᴜᴛᴜʙᴇ ꜱᴏɴɢ", _0x3885cc))
@@ -787,7 +788,7 @@ cmd({
       return await _0x1c8285.reply("*_Give Me Search Query!_*");
     }
     let _0x2878ec = await yts(_0xca939c);
-    let _0x4186e4 = "*QUEEN_ANITA-V2 • SONG DOWNLOADER* \n*_______________________________* \n\n_Reply Any Number To Download._\n  _For Audio: 1 mp3._\n  _For Video: 1 video._\n  _For document: 1 document._\n\n_Results For : " + _0xca939c + "_ \n\n";
+    let _0x4186e4 = "*NTEEJ-MD • SONG DOWNLOADER* \n*_______________________________* \n\n_Reply Any Number To Download._\n  _For Audio: 1 mp3._\n  _For Video: 1 video._\n  _For document: 1 document._\n\n_Results For : " + _0xca939c + "_ \n\n";
     let _0x463366 = 1;
     for (let _0x308e22 of _0x2878ec.all) {
       _0x4186e4 += " \n*" + _0x463366++ + " : " + _0x308e22.title + (_0x308e22.timestamp ? "(" + _0x308e22.timestamp + ")" : "") + "*\n*Url : " + _0x308e22.url + "*";
@@ -1005,7 +1006,7 @@ smd({
         url: _0x59bbaa
       },
       mimetype: "audio/mpeg",
-      fileName: "QUEEN_ANITA-V2--" + _0x1d542b[1] + ".mp3",
+      fileName: "NTEEJ-MD--" + _0x1d542b[1] + ".mp3",
       caption: Config.caption,
       contextInfo: _0x10e2fa
     };
@@ -1026,7 +1027,7 @@ cmd({
 }) => {
   if (_0xb75e78.quoted && _0xb75e78.text) {
     const _0x5b8ee5 = _0xb75e78.quoted.text.split("\n");
-    if (_0x5b8ee5[0].includes("QUEEN_ANITA-V2 • SONG DOWNLOADER")) {
+    if (_0x5b8ee5[0].includes("NTEEJ-MD • SONG DOWNLOADER")) {
       const _0x1724ba = _0x5b8ee5.find(_0x525632 => _0x525632.startsWith("Url :"));
       let _0x43a95e = _0x1724ba.replace("Url :", "").trim();
       try {
@@ -1079,7 +1080,7 @@ cmd({
       } catch (_0x189dd8) {
         return await _0xb75e78.reply("Error While Downloading Video : " + _0x189dd8);
       }
-    } else if (_0x5b8ee5[0].includes("QUEEN_ANITA-V2 • SONG DOWNLOADER")) {
+    } else if (_0x5b8ee5[0].includes("NTEEJ-MD • SONG DOWNLOADER")) {
       let _0x307bb6 = "*" + _0xb75e78.text.split(" ")[0] + " : ";
       const _0x56275d = _0x5b8ee5.find(_0x3b5e74 => _0x3b5e74.startsWith(_0x307bb6));
       if (_0x56275d) {
@@ -1119,7 +1120,7 @@ cmd({
           _0xb75e78.error(_0x3de0e2 + "\n\nCommand yts Listener", _0x3de0e2, "*Video Not Found!*");
         }
       }
-    } else if (_0x5b8ee5[0].includes("QUEEN_ANITA-V2 • SONG DOWNLOADER")) {
+    } else if (_0x5b8ee5[0].includes("NTEEJ-MD • SONG DOWNLOADER")) {
       let _0x35d668 = "*" + _0xb75e78.text.split(" ")[0] + " : ";
       const _0x205a5f = _0x5b8ee5.find(_0x304058 => _0x304058.startsWith(_0x35d668));
       if (_0x205a5f) {
