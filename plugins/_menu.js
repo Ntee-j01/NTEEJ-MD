a09b57670a359f062eb9ae2d8565b791d881bf1b
Nteej <140890199+Ntee-j01@users.noreply.github.com>
2024-06-28 17:24:20 +0200

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -150,9 +150,9 @@ astro_patch.smd(
   ${menuThemeCommandPrefix} *ᴄᴏᴍᴍᴀɴᴅs:* ${commands.length}
   ${menuThemeCommandPrefix} *ᴜsᴀɢᴇ ᴛʀᴇɴᴅ:* ${trend_usage}
   ${menuThemeCommandPrefix} *ᴅᴀᴛᴀʙᴀsᴇ:* ${database_info}
-  ${menuThemeFooter}\n                   ┌┤✑  Thanks for Choosing QUEEN_ANITA-V2
+  ${menuThemeFooter}\n                   ┌┤✑  Thanks for Choosing NTEEJ-MD 👍
 │└────────────┈ ⳹        
-│*©2024-2099 David Cyril*
+│*©2024 NTEEJ *
 └─────────────────┈ ⳹
   \n${readmore}\n`;
 
