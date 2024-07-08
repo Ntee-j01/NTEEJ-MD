45ed5cefef4016d08b3de17913d2e3ebe4c7c17f
zeecole <143521047+zeecole@users.noreply.github.com>
2024-07-08 22:07:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (fs.existsSync(".env"))
   require("dotenv").config({ path: __dirname + "/.env" });
 
 //=======[dependencies]====================//
-global.SESSION_ID = process.env.SESSION_ID || "";
+global.SESSION_ID = process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0M3MVZUbk5zREU3WXp6UkVlS21pcTE2Vy9SSFFSZjlxeENsaVYvbTBXWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlVHeTBnVUhBVXJxeXpHbTRPKzFCbVE1TjlsZnJsU05KKzRTMzhhOCtTRT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJFTGF3cW9VV2gyUXBNNHFLSmc4MldSQ0VBU2RJdU8xWmRGS2JZWXVSQUZBPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJLNWtqL1pFYXA5VDBlUzhlcFhaNk9WdjBBeEo5VjdPRXFqbFJ3dTk1K0QwPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IndCQi9SYzJUdFRoeTRmaklNbGMwQUwvdHRJaWdQMS9tc2d4Ymxpd0FtbTQ9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImJDTFVLZk11UWlubFpxYnFXNFdXMFp6Z2F1UCthSC9idy9CeVk4NlJkVFU9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiY0lTWVc4Z3Rxd245WVo2YStYdEQ2L01iN1NBZnNiekFibDk5NUhjNjkwTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVUJGNEN1Nm4wUVRYN2gzN3huSnUxc0dsd2JaeXk5V29oZWYvWjNBZjBIMD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkYyazNERFc1SnNLRGNNWksrdXRFRk5NSzF6RWVOSWFBb3hiTGVyd21jY1ZvM1ZJR3pVRGc2NXVUTDl2RXBkQlhFa0syWHNVakJMYVBFcXUvODZlNGp3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTIzLCJhZHZTZWNyZXRLZXkiOiJNRExCSFM2NG91VlpNVmNoQTU5WjQ3c3hkM21rNkowVU53YXdmRVRiTHlzPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJaZFUycnJWLVN2NkpKcUVZcVlOWTVBIiwicGhvbmVJZCI6Ijg1Nzk0MDI1LTMzOTEtNGEzNC04OTFhLWUwYjIzNmI3NmU0YiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJEd2VFTjFMSThMb0ljL1BzWE9GUmpoaUVXWUU9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicjZXTVF5S3N4YmxTRmVkcHlZRnZJRFRKTVJZPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IjY2Njc5RFFUIiwibWUiOnsiaWQiOiIyMzQ4MTcxNDM1NzY1OjU5QHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNLRHprUDhFRUtpZHNiUUdHQTRnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJlUCtBNFkxYUl6bnZjM0tnNWRZQTRJdGtjeWVVdUpQVEk5V3BHRW8xSFJBPSIsImFjY291bnRTaWduYXR1cmUiOiJaOFZNZEdaN04rUXhHeDRJQjdDZjJ0YTFBQVFvY2tpQVdpdmI0UkIrYzhrOVV1OFpFUmJPRnkwcUFZM3pUUExWdEpDTDA2UWFyNkZvdXRUNUw5UEdEQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiblBHb3dtaXVOWkcxbVAzbE5vY0luY20vd2I1YkdXY1d1V1FHUnEyQldmSHBFckk5Um1FSkpGUjZaVnI0WHZpbk01SEFIUTF6UGVVMkxwNHVoeGRyaFE9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyMzQ4MTcxNDM1NzY1OjU5QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlhqL2dPR05XaU01NzNOeW9PWFdBT0NMWkhNbmxMaVQweVBWcVJoS05SMFEifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MjA0NzEyMjF9";
 global.MONGODB = process.env.MONGODB_URI || "";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.sudo = process.env.SUDO
@@ -61,14 +61,14 @@ global.session_reset = process.env.SS_RESET || "false";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
 global.warncount = process.env.WARN_COUNT || 3;
-global.disablepm = process.env.DISABLE_PM || "false";
+global.disablepm = process.env.DISABLE_PM || "true";
 (global.disablegroup = process.env.DISABLE_GROUPS || "false"),
   (global.MsgsInLog = process.env.MSGS_IN_LOG || "true");
 global.waPresence = process.env.WAPRESENCE || "null";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null";
-global.read_status = process.env.AUTO_READ_STATUS || "false";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "null";
 global.read_status_from = process.env.READ_STATUS_FROM || "null";
